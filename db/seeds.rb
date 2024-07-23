878b20ac496b952299160e03dc90822e20d3bf6c
StephenWilkens <stephenwilkens@gmail.com>
2024-07-22 22:09:42 -0400

    update customer bar boolean and seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,60 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'lead_seeds.csv'))
+csv = CSV.parse(csv_text, headers: true, encoding: 'ISO-8859-1')
+csv.each do |row|
+  c = Customer.new
+  c.name = row['Name']
+  if row['Street Address'] != nil
+    if row['Street Address'].split(',')[0] != nil
+      c.address = row['Street Address'].split(',')[0]
+    else
+      c.address = ""
+    end
+    if row['Street Address'].split(',')[1] != nil
+      c.city = row['Street Address'].split(",")[1]
+    else
+      c.city = ""
+    end
+    if row['Street Address'].split(",")[2] != nil
+      if row['Street Address'].split(",")[2].strip.split(" ")[0] != nil
+        c.state = row['Street Address'].split(",")[2].strip.split(" ")[0]
+      else
+        c.state = ""
+      end
+      if row['Street Address'].split(",")[2].strip.split(" ")[1] != nil
+        c.zip_code = row['Street Address'].split(",")[2].strip.split(" ")[1]
+      else
+        c.zip_code = ""
+      end
+    end
+  else
+    c.address = ""
+    c.city = ""
+    c.state = ""
+    c.zip_code = ""
+  end
+  c.county = row['County']
+  c.point_of_contact = row['Contact']
+  c.contact_information = row['Contact Info']
+  if row['Last Check-in'] != nil
+
+    c.last_check_in = Date.strptime(row['Last Check-in'], '%m/%d/%Y')
+  else
+    c.last_check_in = ""
+  end
+  if row['Next Check-in'] != nil
+
+    c.next_check_in = Date.strptime(row['Next Check-in'], '%m/%d/%Y')
+  else
+    c.next_check_in = ""
+  end
+  c.notes = row['Notes']
+  c.interest_level = row['Interest']
+  c.liquor_store = row['Liquor Store']
+  c.bar = row['Bar']
+  c.save
+  puts "#{c.name} saved"
+end
+
+puts "There are now #{Customer.count} rows in the customers table"
--- spec/factories/customers.rb
+++ spec/factories/customers.rb
@@ -1,18 +1,18 @@
 FactoryBot.define do
   factory :customer do
-    name { "MyString" }
-    address { "MyString" }
-    city { "MyString" }
-    county { "MyString" }
-    state { "MyString" }
-    point_of_contact { "MyString" }
-    contact_information { "MyString" }
-    zip_code { "MyString" }
-    last_check_in { "MyString" }
-    next_check_in { "2024-07-22" }
-    notes { "MyText" }
-    interest_level { 1 }
-    bar { "MyString" }
-    liquor_store { false }
+    name { Faker::Company.name }
+    address { Faker::Address.street_address }
+    city { Faker::Address.city }
+    county { Faker::Address.community }
+    state { Faker::Address.state }
+    point_of_contact { Faker::Name.first_name }
+    contact_information { Faker::PhoneNumber.phone_number }
+    zip_code { Faker::Address.zip_code }
+    last_check_in { Faker::Date.between(from: '2024-03-01', to: Date.today) }
+    next_check_in { Faker::Date.between(from: Date.today, to: 1.month.from_now) }
+    notes { Faker::Lorem.paragraph }
+    interest_level { Faker::Number.between(from: 0, to: 5) }
+    bar { Faker::Boolean.boolean }
+    liquor_store { Faker::Boolean.boolean }
   end
 end
